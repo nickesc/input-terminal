0cc6e80b1400626dba9656096943b82114e12811
N. Escobar <nick@nescobar.media>
2025-07-17 00:18:08 -0400

    update and add push history tests

--- tests/input-terminal.test.ts
+++ tests/input-terminal.test.ts
@@ -99,13 +99,42 @@ describe('input-terminal', () => {
         const term: Terminal = new Terminal(test_input, test_history, "", "");
         expect(term.get_history()).toBe(test_history);
     });
-    it('should push command to history',  async () => {
+
+    // PUSH HISTORY TESTS
+    it('should return the length of the command history on push',  async () => {
         const dom: JSDOM = new JSDOM(`<!DOCTYPE html><input id=="test"></input>`);
         const test_input = dom.window.document.getElementById("test") as HTMLInputElement;
         const test_command: Command = {user_input: ["test"]};
 
         const term: Terminal = new Terminal(test_input);
         expect(term.push_history(test_command)).toBe(1);
+    });
+    it('should push command to history',  async () => {
+        const dom: JSDOM = new JSDOM(`<!DOCTYPE html><input id=="test"></input>`);
+        const test_input = dom.window.document.getElementById("test") as HTMLInputElement;
+        const test_commands: Command[] = [{user_input: ["test1"]}, {user_input: ["test2"]}];
+
+        const term: Terminal = new Terminal(test_input);
+        term.push_history(test_commands[0]);
+        term.push_history(test_commands[1]);
+        expect(term.get_history()).toEqual([test_commands[1],test_commands[0]]);
+    });
+    it('should retain index in history if command is pushed',  async () => {
+        const dom: JSDOM = new JSDOM(`<!DOCTYPE html><input id=="test"></input>`);
+        const test_input = dom.window.document.getElementById("test") as HTMLInputElement;
+        const test_commands: Command[] = [{user_input: ["test1"]}, {user_input: ["test2"]}, {user_input: ["test3"]}];
+
+        const term: Terminal = new Terminal(test_input);
+        term.push_history(test_commands[0]);
+        expect(term.current_history()).toBe(undefined);
+        term.push_history(test_commands[1]);
+        expect(term.current_history()).toBe(undefined);
+
+        expect(term.previous_history()).toBe(test_commands[1]);
+
+        term.push_history(test_commands[2]);
+        expect(term.current_history()).toBe(test_commands[1]);
+        expect(term.previous_history()).toBe(test_commands[0]);
     });
 
 
