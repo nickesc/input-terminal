6387a5cc1fbc78b4d17c9d4e964d0d7166e81808
N. Escobar <nick@nescobar.media>
2025-07-23 02:48:48 -0400

    update demo site

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,17 @@
 import { Terminal, ExitObject, Command } from './input-terminal.js';
 
+let output = document.getElementById("output");
 let terminal = new Terminal(document.getElementById("terminal"));
 terminal.init();
 
-terminal.history.push(new ExitObject(["test1"], undefined, 0))
-terminal.history.push(new ExitObject(["test2"], undefined, 0))
-terminal.history.push(new ExitObject(["test3"], undefined, 0))
+terminal.history.push(new ExitObject(["help"], undefined, 0))
+terminal.history.push(new ExitObject(["result"], undefined, 0))
+terminal.history.push(new ExitObject(["echo this is a test"], undefined, 0))
 
-terminal.commands.add(new Command("help", (args, options, terminal) => {console.log("help")}))
-terminal.commands.add(new Command("echo", (args, options, terminal) => {console.log(args.join(" "), options.join(" "))}))
-terminal.commands.add(new Command("git", (args, options, terminal) => {console.log(terminal)}))
+terminal.commands.add(new Command("help", (args, options, terminal) => {output.innerText = "Commands:\necho\nhelp\nresult"}))
+terminal.commands.add(new Command("echo", (args, options, terminal) => {
+    output.innerText = `${args.join(" ")} ${options.join(" ")}`
+}))
+terminal.commands.add(new Command("result", (args, options, terminal) => {
+    output.innerText = `${terminal.lastExitCode}`
+}))
