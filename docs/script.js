e265efcc8b8d785cfcd5535278df35ceb041e939
N. Escobar <nick@nescobar.media>
2025-07-30 21:41:16 -0400

    fix echo command

--- docs/script.js
+++ docs/script.js
@@ -8,9 +8,11 @@ terminal.history.push(new ExitObject(["help"], undefined, 0))
 terminal.history.push(new ExitObject(["result"], undefined, 0))
 terminal.history.push(new ExitObject(["echo this is a test"], undefined, 0))
 
-terminal.commands.add(new Command("help", (args, options, terminal) => {output.innerText = "Commands:\necho\nhelp\nresult"}))
+terminal.commands.add(new Command("help", (args, options, terminal) => {
+    output.innerText = "Commands:\necho\nhelp\nresult"
+}))
 terminal.commands.add(new Command("echo", (args, options, terminal) => {
-    output.innerText = `${args.join(" ")} ${options.join(" ")}`
+    output.innerText = `${args.join(" ")}`
 }))
 terminal.commands.add(new Command("result", (args, options, terminal) => {
     output.innerText = `${terminal.lastExitCode}`
