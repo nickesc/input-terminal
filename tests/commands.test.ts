e79bf988b7d896327526356e70d732cc59b791d3
N. Escobar <nick@nescobar.media>
2025-07-31 10:51:42 -0400

    reorganized command tests and added exit object tests

--- tests/commands.test.ts
+++ tests/commands.test.ts
@@ -5,6 +5,10 @@ function isCommand(target: any): boolean {
     return target instanceof Command;
 }
 
+function isExitObject(target: any): boolean {
+    return target instanceof ExitObject;
+}
+
 function isTermCommands(target: any): boolean {
   return target instanceof TermCommands;
 }
@@ -21,17 +25,6 @@ describe('TermCommands Tests', () => {
         test_command = tCommand("test0");
         test_commands = [tCommand("test1"), tCommand("test2"), tCommand("test3")];
     });
-
-    // COMMAND TESTS
-    it('should construct a Command object',  () => {
-        const command: Command = new Command("test", () => {return {}});
-        expect(isCommand(command)).toBe(true);
-    });
-    it('should construct with a key',  () => {
-        const command: Command = new Command("test", () => {return {}});
-        expect(command.key).toEqual("test");
-    });
-
 
     // CONSTRUCTION TEST
     it('should construct a TermCommands object',  () => {
@@ -84,5 +77,38 @@ describe('TermCommands Tests', () => {
         const removed_command: Command | undefined = commands.remove(test_command);
         expect(commands.list).toEqual(test_commands);
         expect(removed_command).toEqual(undefined);
+    });
+});
+
+describe('Command Tests', () => {
+    // COMMAND TESTS
+    it('should construct a Command object',  () => {
+        const command: Command = new Command("test", () => {return {}});
+        expect(isCommand(command)).toBe(true);
+    });
+    it('should construct with a key',  () => {
+        const command: Command = new Command("test", () => {return {}});
+        expect(command.key).toEqual("test");
+    });
+});
+
+describe('ExitObject Tests', () => {
+    beforeEach(() => {
+        test_command = tCommand("test0");
+        test_commands = [tCommand("test1"), tCommand("test2"), tCommand("test3")];
+    });
+    // EXIT OBJECT TESTS
+    it('should construct an exit object',  () => {
+        const exit_object: ExitObject = new ExitObject([], test_command, 0, {});
+        expect(isExitObject(exit_object)).toBe(true);
+    });
+
+    it('should construct an exit object',  () => {
+        const exit_object: ExitObject = new ExitObject([], test_command, 0, {});
+        expect(exit_object.command).toEqual(test_command);
+        expect(exit_object.timestamp).toBeDefined();
+        expect(exit_object.exit_code).toEqual(0);
+        expect(exit_object.user_input).toEqual([]);
+        expect(exit_object.output).toEqual({});
     });
 });
