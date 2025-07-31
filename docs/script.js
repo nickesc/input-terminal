6975918cd36076f7961694d8919d90142b90e33d
N. Escobar <nick@nescobar.media>
2025-07-31 19:44:28 -0400

    update demo code

--- docs/script.js
+++ docs/script.js
@@ -4,19 +4,33 @@ let output = document.getElementById("output");
 let terminal = new Terminal(document.getElementById("terminal"));
 terminal.init();
 
-terminal.history.push(new ExitObject(["help"], undefined, 0))
-terminal.history.push(new ExitObject(["result"], undefined, 0))
-terminal.history.push(new ExitObject(["echo this is a test"], undefined, 0))
+const help_command = new Command("help", (args, options, terminal) => {
+    output.innerText = "Commands:\necho\nhelp\nresult";
+    return {};
+});
 
-terminal.commands.add(new Command("help", (args, options, terminal) => {
-    output.innerText = "Commands:\necho\nhelp\nresult"
-}))
-terminal.commands.add(new Command("echo", (args, options, terminal) => {
-    output.innerText = `${args.join(" ")}`
-}))
-terminal.commands.add(new Command("echo2", (args, options, terminal) => {
-    output.innerText = `${args.join(" ")}`
-}))
-terminal.commands.add(new Command("result", (args, options, terminal) => {
-    output.innerText = `${terminal.lastExitCode}`
-}))
+const result_command = new Command("result", (args, options, terminal) => {
+    output.innerText = `${terminal.lastExitCode}`;
+    return {};
+});
+
+const return_command = new Command("return", (args, options, terminal) => {
+    return {args: args, options: options};
+});
+
+const echo_command = new Command("echo", (args, options, terminal) => {
+    output.innerText = `${args.join(" ")}`;
+    return {};
+});
+
+terminal.commands.add(help_command);
+terminal.commands.add(result_command);
+terminal.commands.add(return_command);
+terminal.commands.add(echo_command);
+
+terminal.history.push(new ExitObject(["help"], help_command, 0, {}))
+terminal.history.push(new ExitObject(["result"], result_command, 0, {}))
+terminal.history.push(new ExitObject(["return"], return_command, 0, {}))
+terminal.history.push(new ExitObject(["echo this is a test"], echo_command, 0, {}))
+
+
