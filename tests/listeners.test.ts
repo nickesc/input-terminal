649c888aa95f2c228ff4870ab15aeada92dcb2dc
N. Escobar <nick@nescobar.media>
2025-08-06 12:47:41 -0400

    add tests for custom keys

--- tests/listeners.test.ts
+++ tests/listeners.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 import { JSDOM } from 'jsdom';
-import { Terminal, ExitObject } from '../src/input-terminal';
-import { Command } from '../src/commands';
+import { Terminal, ExitObject, TermOptions, Command, built_ins } from '../src/input-terminal';
 
 describe('TermListeners Tests', () => {
     let terminal: Terminal;
@@ -42,6 +41,66 @@ describe('TermListeners Tests', () => {
         const event = new dom.window.KeyboardEvent('keydown', { key: 'Enter' });
         input.dispatchEvent(event);
         expect(terminal.get_input_value()).toBe("");
+    });
+
+    it('should handle autocomplete key', () => {
+        terminal.update_input('xyz');
+        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('xyz');
+    });
+    it('should handle autocomplete key with single prediction', () => {
+        terminal.bin.add(new Command('testcmd', (args, options, terminal) => {return true;}));
+        terminal.update_input('test');
+        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('testcmd');
+    });
+    it('should handle autocomplete key with multiple predictions', () => {
+        terminal.bin.add(new Command('test1', (args, options, terminal) => {return true;}));
+        terminal.bin.add(new Command('test2', (args, options, terminal) => {return true;}));
+        terminal.bin.add(new Command('test3', (args, options, terminal) => {return true;}));
+        terminal.update_input('test');
+
+        let event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test1');
+
+        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test2');
+
+        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test3');
+
+        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test1');
+    });
+
+    it('should handle autocomplete key with empty input', () => {
+        terminal.update_input('');
+        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe(terminal.bin.list[0]?.key || "");
+    });
+    it('should reset autocomplete predictions when typing other keys', () => {
+        terminal.bin.add(new Command('test1', (args, options, terminal) => {return true;}));
+        terminal.bin.add(new Command('test2', (args, options, terminal) => {return true;}));
+        terminal.update_input('test');
+
+        let event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test1');
+
+        terminal.update_input('test1a');
+        event = new dom.window.KeyboardEvent('keydown', { key: 'a' });
+        input.dispatchEvent(event);
+
+        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
+        input.dispatchEvent(event);
+        expect(terminal.get_input_value()).toBe('test1a');
     });
 
     it('should prevent backspace on prompt', () => {
@@ -106,63 +165,51 @@ describe('TermListeners Tests', () => {
         input.dispatchEvent(event);
         expect(input.selectionStart).toBe((`${terminal.options.preprompt}${terminal.options.prompt}`).length);
     });
+});
+describe("Custom Key Tests", () => {
 
-    it('should handle autocomplete key with single prediction', () => {
-        terminal.bin.add(new Command('testcmd', (args, options, terminal) => {return true;}));
-        terminal.update_input('test');
-        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('testcmd');
+    let terminal: Terminal;
+    let input: HTMLInputElement;
+    let dom: JSDOM;
+
+    const customOptions = {
+        previousKey: "Up",
+        nextKey: "Down",
+        returnKey: "Space",
+        autocompleteKey: "Shift",
+        installBuiltIns: false
+    };
+
+    let historyCommand = new ExitObject(['echo test'], 'echo test', undefined, 0, 'test')
+
+    dom = new JSDOM('<!DOCTYPE html><html><body><input type="text" id="terminal-input"></body></html>');
+    global.document = dom.window.document;
+    input = document.getElementById('terminal-input') as HTMLInputElement;
+    terminal = new Terminal(input, customOptions);
+
+    terminal.history.push(historyCommand);
+    terminal.bin.list = [built_ins[0]]
+    terminal.init();
+
+    it('should handle a custom previous key', () => {
+        let previousEvent = new dom.window.KeyboardEvent('keydown', { key: customOptions.previousKey });
+        input.dispatchEvent(previousEvent);
+        expect(terminal.get_input_value()).toBe(historyCommand.raw_input);
     });
-    it('should handle autocomplete key with multiple predictions', () => {
-        terminal.bin.add(new Command('test1', (args, options, terminal) => {return true;}));
-        terminal.bin.add(new Command('test2', (args, options, terminal) => {return true;}));
-        terminal.bin.add(new Command('test3', (args, options, terminal) => {return true;}));
-        terminal.update_input('test');
-
-        let event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test1');
-
-        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test2');
-
-        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test3');
-
-        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test1');
+    it('should handle a custom next key', () => {
+        let nextEvent = new dom.window.KeyboardEvent('keydown', { key: customOptions.nextKey });
+        input.dispatchEvent(nextEvent);
+        expect(terminal.get_input_value()).toBe("");
     });
-    it('should handle autocomplete key with no predictions', () => {
-        terminal.update_input('xyz');
-        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('xyz');
+    it('should handle a custom return key', () => {
+        let returnEvent = new dom.window.KeyboardEvent('keydown', { key: customOptions.returnKey });
+        terminal.update_input(historyCommand.raw_input)
+        input.dispatchEvent(returnEvent);
+        expect(terminal.get_last_exit_object()?.raw_input).toBe(historyCommand.raw_input);
     });
-    it('should handle autocomplete key with empty input', () => {
-        terminal.update_input('');
-        const event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe(terminal.bin.list[0]?.key || "");
-    });
-    it('should reset autocomplete predictions when typing other keys', () => {
-        terminal.bin.add(new Command('test1', (args, options, terminal) => {return true;}));
-        terminal.bin.add(new Command('test2', (args, options, terminal) => {return true;}));
-        terminal.update_input('test');
-
-        let event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test1');
-
-        terminal.update_input('test1a');
-        event = new dom.window.KeyboardEvent('keydown', { key: 'a' });
-        input.dispatchEvent(event);
-
-        event = new dom.window.KeyboardEvent('keydown', { key: 'Tab' });
-        input.dispatchEvent(event);
-        expect(terminal.get_input_value()).toBe('test1a');
+    it('should handle a custom autocomplete key', () => {
+        let autocompleteEvent = new dom.window.KeyboardEvent('keydown', { key: customOptions.autocompleteKey });
+        input.dispatchEvent(autocompleteEvent);
+        expect(terminal.get_input_value()).toBe(built_ins[0].key);
     });
 });
