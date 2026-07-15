4b9eb2c7f94754b17af04285f44d6f44b32283e7
N. Escobar <nick@nickesc.io>
2026-07-15 18:12:21 -0400

    remove termoptionsconfig and update termoptions to be interface w object

--- src/options.ts
+++ src/options.ts
@@ -2,88 +2,75 @@
  * The configuration for the terminal.
  * @category Terminal Components
  */
-export interface TermOptionsConfig {
+export interface TermOptions {
     /**
      * The key used to select the previous command.
      * @default "ArrowUp"
      */
-    previousKey?: string;
+    readonly previousKey: string;
 
     /**
      * The key used to select the next command.
      * @default "ArrowDown"
      */
-    nextKey?: string;
+    readonly nextKey: string;
 
     /**
      * The key used to enter a command.
      * @default "Enter"
      */
-    returnKey?: string;
+    readonly returnKey: string;
 
     /**
      * The key used to autocomplete a command.
      * @default "Tab"
      */
-    autocompleteKey?: string;
+    readonly autocompleteKey: string;
 
     /**
      * Whether the terminal should install built-in commands.
      * @default true
      */
-    installBuiltins?: boolean;
+    readonly installBuiltins: boolean;
 
     /**
      * A line of text that is displayed at the beginning of the command line.
      * @default "> "
      */
-    prompt?: string;
+    readonly prompt: string;
 
     /**
      * A line of text that is displayed before the prompt.
      * @default ""
      */
-    preprompt?: string;
+    readonly preprompt: string;
 
     /**
      * Whether the terminal should add a command with empty input to history.
      * @default false
      */
-    addEmptyCommandToHistory?: boolean;
+    readonly addEmptyCommandToHistory: boolean;
 
     /**
      * Whether the terminal should add a command with duplicate input (same as the last command's input) to history.
      * @default false
      */
-    showDuplicateCommands?: boolean;
+    readonly showDuplicateCommands: boolean;
 
     /**
      * Any other custom options.
      */
-    [key: string]: any;
+    readonly [key: string]: unknown;
 }
 
-/**
- * Manages the terminal's configuration.
- * @category Terminal Components
- */
-export class TermOptions implements TermOptionsConfig {
-    public previousKey: string = "ArrowUp";
-    public nextKey: string = "ArrowDown";
-    public returnKey: string = "Enter";
-    public autocompleteKey: string = "Tab";
-    public installBuiltins: boolean = true;
-    public prompt: string = "> ";
-    public preprompt: string = "";
-    public addEmptyCommandToHistory: boolean = false;
-    public showDuplicateCommands: boolean = false;
-
-    /**
-     * @param {TermOptionsConfig} [options] - an optional configuration to initialize the terminal with
-     */
-    constructor(options?: TermOptionsConfig) {
-        if (options) {
-            Object.assign(this, options);
-        }
-    }
-}
+export const defaultTermOptions: TermOptions = Object.freeze({
+    previousKey: "ArrowUp",
+    nextKey: "ArrowDown",
+    returnKey: "Enter",
+    autocompleteKey: "Tab",
+    installBuiltins: true,
+    prompt: "> ",
+    preprompt: "",
+    addEmptyCommandToHistory: false,
+    showDuplicateCommands: false,
+});
