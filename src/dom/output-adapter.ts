2ad6fe1591fbfb4656e019c7d9db56dd6e20f59e
N. Escobar <nick@nickesc.io>
2026-08-15 18:32:25 -0400

    update dom adapter to implement the command method

--- src/dom/output-adapter.ts
+++ src/dom/output-adapter.ts
@@ -5,12 +5,17 @@ import type {OutputAdapter, OutputMetadata} from "../output-adapter.ts";
  * @category Terminal Output
  */
 export class DOMOutputAdapter implements OutputAdapter {
+    private static readonly COMMAND_CLASS = "input-terminal-command";
     private static readonly STDOUT_CLASS = "input-terminal-stdout";
     private static readonly STDERR_CLASS = "input-terminal-stderr";
     private readonly element: HTMLElement;
 
     constructor(element: HTMLElement) {
         this.element = element;
+    }
+
+    public command(data: string, metadata: OutputMetadata): void {
+        this.append(data, DOMOutputAdapter.COMMAND_CLASS, metadata);
     }
 
     public stdout(data: unknown, metadata: OutputMetadata): void {
